3c3cc2bf13470beaeb702410775e73af999401ee
Bachir47 <bazemlal47@gmail.com>
2022-01-04 22:35:18 +0100

    Question 2

--- Mat2D.java
+++ Mat2D.java
@@ -1,5 +1,16 @@
 public class Mat2D
 {
 	private double T[][];
-	
+	Mat2D(double a[][]) //Constructeur
+	{
+		T=new double [a.length][];
+		for(int i=0; i<a.length; i++)
+		{
+			T[i]=new double [a[i].length];
+			for(int j=0; j<a[i].length;j++)
+			{
+				T[i][j]=a[i][j];
+			}
+		}
+	}
 }
